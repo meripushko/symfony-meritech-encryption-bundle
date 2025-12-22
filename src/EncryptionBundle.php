3439f2ff7c7268b25669927c2d24447ae1a429dd
Simeon Meripushkoski <sime.meripushkoski@gmail.com>
2025-12-22 17:06:38 +0100

    Updated composer files with version and enhanced EncryptionBundle documentation

--- src/EncryptionBundle.php
+++ src/EncryptionBundle.php
@@ -4,6 +4,12 @@ namespace Meritech\EncryptionBundle;
 
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
+/**
+ * A Bundle that provides the Encrypted attribute for Doctrine entities.
+ * This attribute allows transparent AES-256-GCM encryption and decryption.
+ *
+ * @author Simeon Meripushkoski <sime.meripushkoski@gmail.com>
+ */
 class EncryptionBundle extends AbstractBundle
 {
 }
